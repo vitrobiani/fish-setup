a4f9b0f088ecff2926728e66c93b8e6ae2aa664a
vitrobiani <vitrobiani@gmail.com>
2024-01-27 13:40:26 +0200

    added a lot of aliasses

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias ll 'ls -al'
+    alias tm 'tmux'
+    alias tma 'tmux a'
     alias nv 'nvim'
     alias nv. 'nvim .'
     alias nvconf 'set x (pwd) && cd ~/.config/nvim/ && nv. && cd $x'
@@ -11,4 +13,7 @@ if status is-interactive
     alias gc 'git commit -a -m "jtp"'
     alias gp 'git push'
     alias gacp 'ga && gc && gp origin main'
+    alias q 'exit'
+    alias m 'echo (pwd)>~/.config/fish/loc.txt'
+    alias tp 'cd (cat ~/.config/fish/loc.txt)'
 end
